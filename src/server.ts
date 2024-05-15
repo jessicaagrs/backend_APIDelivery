0f865a7ba078d67655facf2253e70f79dc5e1ca7
Jessica Aguiar <jessicaag.rs@gmail.com>
2024-05-15 17:55:15 -0300

    feat: adding swagger to api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,25 @@
 import Fastify from "fastify";
 import { routes } from "./app";
 import cors from "@fastify/cors";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 
 const app = Fastify({ logger: true });
 
 const start = async () => {
+	await app.register(swagger);
+	await app.register(swaggerUI, {
+		routePrefix: "/",
+	});
+	await app.register(cors);
+	await app.register(routes, { prefix: "/v1" });
 
-  await app.register(cors);
-  await app.register(routes);
-
-  try {
-    await app.listen({ port: 3333 });
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+	try {
+		await app.listen({ port: 3333 });
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
 };
 
 start();
